f819dc71791e226f8d8f537a5afa72d2f579fa26
Henadzi Arashkevich <oreshkevich85@mail.ru>
2023-02-18 14:39:31 +0300

    feat: add website-2

---
+++ learnjs-tasks/chapter-2/chapter-2.4/script.js
@@ -0,0 +1,17 @@
+'use strict';
+// task 1
+let admin;
+let nameUser = 'Джон';
+admin = nameUser;
+alert(admin);
+
+// task 2
+
+let planetEarth = 'Земля';
+let userName = 'Джон';
+
+// task 3
+
+const BIRTHDAY = '18.04.1982';
+
+const age = someCode(BIRTHDAY);
---
+++ learnjs-tasks/chapter-2/chapter-2.6/script.js
@@ -0,0 +1,4 @@
+'use strict';
+// task 1
+let nameUser = prompt('Ваше имя?', '');
+alert(nameUser);
